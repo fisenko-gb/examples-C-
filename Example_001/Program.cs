5550dfa7dc1dfd61a40777ac365c7b7d6a2aa4ed
Fisenko <fisenko-a@yandex.ru>
2022-06-06 17:15:55 +0400

    name

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,5 +1,13 @@
 ﻿
 Console.Write("Введите свое имя: ");
 string i = Console.ReadLine();
-Console.WriteLine("Привет, " + i + "!!!");
+
+if (i.ToLower() == "маша")
+{
+    Console.WriteLine("Мария, ты ли это!?!?!?!");
+}else
+{
+    Console.WriteLine("Привет, " + i + "!!!");    
+}
+
 
